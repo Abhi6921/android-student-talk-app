52fcbeea673373b0e247508a49cc847f845b73f7
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2022-05-27 23:20:12 +0200

    Initial commit

---
+++ app/src/main/java/com/abhicoder/student_talk_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.abhicoder.student_talk_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/abhicoder/student_talk_app/views/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.abhicoder.student_talk_app.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abhicoder.student_talk_app.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
